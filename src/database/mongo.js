8833818c040907f182d446d3d2d982632d04967e
Fred Zerpa <fredzerpa@gmail.com>
2022-12-15 19:34:26 -0400

    Remove react-dotenv package

--- src/database/mongo.js
+++ src/database/mongo.js
@@ -1,14 +1,12 @@
 import * as Realm from 'realm-web'; 
-import env from "react-dotenv";
-
-export const mongoApp = new Realm.App({ id: env.MONGO_REALM_APP_ID });
-export const mongoClient = mongoApp.currentUser?.mongoClient(env.MONGO_CLUSTER);
-export const mongoDB = mongoClient?.db(env.MONGO_DATABASE);
+export const mongoApp = new Realm.App({ id: process.env.REACT_APP_MONGO_REALM_APP_ID });
+export const mongoClient = mongoApp.currentUser?.mongoClient(process.env.REACT_APP_MONGO_CLUSTER);
+export const mongoDB = mongoClient?.db(process.env.REACT_APP_MONGO_DATABASE);
 
 export const mongoLogIn = async () => {
   // LogIn to MongoDB using API KEY
   const userData = await mongoApp.logIn(
-    Realm.Credentials.apiKey(env.MONGO_REALM_API_KEY)
+    Realm.Credentials.apiKey(process.env.REACT_APP_MONGO_REALM_API_KEY)
   );
 
   return userData;
