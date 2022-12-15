d43aef4cd3de39991c9205f79d7b3e63029cf5e8
Fred Zerpa <fredzerpa@gmail.com>
2022-12-15 19:22:28 -0400

    Remove react-dotenv package

--- public/env.js
+++
@@ -1,6 +0,0 @@
-window.env = {
-  "MONGO_REALM_APP_ID": "data-qqkfy",
-  "MONGO_CLUSTER": "ClusterAngel",
-  "MONGO_DATABASE": "angel",
-  "MONGO_REALM_API_KEY": "jwb2rODg4LWyXHkhvIuwqDghl0QHFFadvHcB0IX3Mvq5VWtrBFaoZCLoHq60NSCz"
-};
